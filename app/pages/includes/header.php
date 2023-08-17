8411eda1d8f96920f3115b52b8ae401b6e4c4d5b
Shuhad Abdul Azeez <shuhad04@gmail.com>
2023-08-17 12:03:16 +0530

    modified

--- app/pages/includes/header.php
+++ app/pages/includes/header.php
@@ -5,53 +5,58 @@
       height: 40px;
       border-radius: 50%;
     }
+
+    .navbar-custom {
+        margin: 0 300px;
+        height: 90px;
+    }
+    .navbar-custom .login-link {
+        margin-left: 45px;
+    }
   </style>
 
-<nav class="navbar navbar-expand-sm navbar-light">
-    <div class="container-fluid">
-      <div>
-        <a href="#" class="navbar-brand text-uppercase">journey</a>
-      </div>
+<nav class="navbar navbar-expand-sm navbar-light navbar-custom">
+    <div class="container-fluid justify-content-between">
+        <div>
+            <a href="#" class="navbar-brand text-uppercase">journey</a>
+        </div>
 
-      <div>
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a href="<?=ROOT?>" class="nav-link text-uppercase">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="<?=ROOT?>/blog" class="nav-link text-uppercase">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a href="<?=ROOT?>/contact" class="nav-link text-uppercase">Contact</a>
-          </li>
-        </ul>
-      </div>
+        <div class="d-flex align-items-center">
+            <ul class="navbar-nav mr-3">
+                <li class="nav-item active">
+                    <a href="<?=ROOT?>" class="nav-link text-uppercase">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?=ROOT?>/blog" class="nav-link text-uppercase">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?=ROOT?>/contact" class="nav-link text-uppercase">Contact</a>
+                </li>
+            </ul>
 
-      <div class="ml-auto">
-            <ul class="navbar-nav">
+            <div class="d-flex align-items-center">
                 <?php if(logged_in()): ?>
-                <li class="nav-item dropdown">
+                <div class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="<?=get_image(user('image'))?>" alt="User Image" class="user-image" style="object-fit: cover;" width="32" height="32">
+                        <img src="<?=get_image(user('image'))?>" alt="User Image" class="user-image" style="object-fit: cover;" width="32" height="32">
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li><a class="dropdown-item" href="<?=ROOT?>/admin">Admin</a></li>
-                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="<?=ROOT?>/logout">Logout</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="<?=ROOT?>/admin">Admin</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="<?=ROOT?>/logout">Logout</a></li>
                     </ul>
-                </li>
+                </div>
                 <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?=ROOT?>/login">Login</a>
-                </li>
+                <div class="nav-item login-link">
+                  <a class="nav-link" href="<?=ROOT?>/login" style="color: #333;">Login</a>
+                </div>
                 <?php endif; ?>  
-            </ul>
+            </div>
         </div>
     </div>
-
-  </nav>
+</nav>
 
   <?php
   
